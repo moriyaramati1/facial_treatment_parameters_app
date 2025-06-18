9f69babc265571beab0e0699afbfde9c8c21f521
Moriya Ramati <Moriyaramati1@gmail.com>
2025-06-18 12:53:40 +0300

    fix select inline

--- src/app/models/handles.ts
+++ src/app/models/handles.ts
@@ -7,13 +7,13 @@ export enum EndimedHandlers {
   Ifine = 'IFINE'
 }
 export enum HifuHandles {
-  H_1_5 = 1.5,
-  H_2 = 2,
-  H_3 = 3,
-  H_4_5 = 4.5,
-  H_6 = 6,
-  H_9 = 9,
-  H_13 = 13,
+  H_1_5 = '1.5',
+  H_2 = '2',
+  H_3 = '3',
+  H_4_5 = '4.5',
+  H_6 = '6',
+  H_9 = '9',
+  H_13 = '13',
   Booster_1_5 = 'בוסטר 1.5',
   Booster_3 = '3',
   Booster_4_5 = 'בוסטר 4.5'
